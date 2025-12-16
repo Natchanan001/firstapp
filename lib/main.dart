9f98d3aa3f6aefca265caad2d716f8531b947a14
Natchanan001 <alwaysforever1112@gmail.com>
2025-12-16 11:40:28 +0700

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'pages/calc.dart';
+import 'pages/contact.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,185 +15,63 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tui Shop',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
+        primarySwatch: Colors.lightBlue,
+        useMaterial3: false
       ),
-      home: const MyHomePage(title: 'Natchanan Shop'),
-      debugShowCheckedModeBanner: false,
+      home: const MyBottomNav(),
+      debugShowCheckedModeBanner: true,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class MyBottomNav extends StatefulWidget {
+  const MyBottomNav({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyBottomNav> createState() => _MyBottomNavState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  // Control Data
-  var price = TextEditingController();
-  var amount = TextEditingController();
-  var receive = TextEditingController();
+class _MyBottomNavState extends State<MyBottomNav> {
 
-  double _total = 0;
-  double _change = 0;
+  int _index = 0;
 
-  // Widgets ---------------------------------------------
 
-  Widget priceTextField() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        controller: price,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: "price per item",
-        ),
-        keyboardType: TextInputType.number,
-      ),
-    );
-  }
-
-  Widget amountTextField() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        controller: amount,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: "amount of items",
-        ),
-        keyboardType: TextInputType.number,
-      ),
-    );
-  }
-
-  Widget receiveMoneyTextField() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        controller: receive,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: "get money",
-        ),
-        keyboardType: TextInputType.number,
-      ),
-    );
-  }
-
-  Widget calculateButton() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(
-        onPressed: () {
-          if (price.text.isNotEmpty && amount.text.isNotEmpty) {
-            setState(() {
-              _total = double.parse(price.text) * double.parse(amount.text);
-            });
-          }
-        },
-        child: const Text("Calculate Total"),
-      ),
-    );
-  }
-
-  Widget showTotalText() {
-    return Text(
-      _total == 0 ? "Total : Baht" : "Total : $_total Baht",
-    );
-  }
-
-  Widget changeCalculateButton() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(
-        onPressed: () {
-          if (receive.text.isNotEmpty) {
-            if (double.parse(receive.text) < _total) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text("Not enough money!")),
-              );
-            } else {
-              setState(() {
-                _change = double.parse(receive.text) - _total;
-              });
-            }
-          }
-        },
-        child: const Text("Calculate Change"),
-      ),
-    );
-  }
-
-  Widget showChangeText() {
-    return Text(
-      _change == 0 ? "Change : Baht" : "Change : $_change Baht",
-    );
-  }
-
-  // -------------------------------------------------------
+  final List<Widget> _pages = [
+    const HomePage(),
+    const CalculatePage(),
+    const ContactPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(
-          widget.title,
-          style: const TextStyle(
-            fontFamily: 'maaja',
-            fontSize: 26,
-            color: Colors.black,
+      body: _pages[_index],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+
+        onTap: (value) {
+          setState(() {
+            _index = value;
+          });
+        },
+
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
           ),
-        ),
-        centerTitle: true,
-      ),
-
-
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-
-          child: Column(
-            children: [
-      Container(
-        padding: const EdgeInsets.symmetric(horizontal: 3, vertical: 3),
-        decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 244, 31, 31),  
-            ),
-        child: const Text(
-          "Calculate",
-        style: TextStyle(
-       fontFamily: 'maaja',
-       fontSize: 28,
-       fontWeight: FontWeight.bold,
-       fontStyle: FontStyle.italic,
-       color: Colors.blue, 
-         ),
-      ),
-    ),
-   Center(
-      child: Image.asset(
-        "assets/delicious-ramen-with-chopsticks.jpg",
-        height: 200,
-      ),
-    ),
-              const SizedBox(height: 20),
-
-              priceTextField(),
-              amountTextField(),
-              calculateButton(),
-              showTotalText(),
-              receiveMoneyTextField(),
-              changeCalculateButton(),
-              showChangeText(),
-            ],
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: "Calculate",
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contact_phone),
+            label: "Contact",
+          ),
+        ],
       ),
     );
   }
